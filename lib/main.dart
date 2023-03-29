932c4b5969cfb117f0364d9d0669448391eb7728
Kacper Płusa <kacper.plusaa@gmail.com>
2023-03-29 20:09:58 +0200

    Add icons to button and style them

--- lib/main.dart
+++ lib/main.dart
@@ -48,54 +48,53 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-        centerTitle: true,
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: ElevatedButton(
+        appBar: AppBar(
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: Text(widget.title),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                ElevatedButton.icon(
                   onPressed: () {
                     print('pick image');
                   },
-                  child: Text('PICK IMAGE'),
+                  label: const Text('PICK IMAGE'),
+                  icon: const Icon(Icons.image),
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color.fromARGB(255, 4, 103, 209),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(16.0)),
+                      minimumSize:
+                          Size(1, MediaQuery.of(context).size.height * 0.05),
+                      textStyle: const TextStyle(fontSize: 20) //////// HERE
+                      ),
                 ),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  print('button pressed!');
-                },
-                child: Text('TAKE PHOTO'),
-              ),
-            ])
-          ],
-        ),
-      )
-    );
+                const SizedBox(
+                  width: 10, // <-- SEE HERE
+                ),
+                ElevatedButton.icon(
+                  onPressed: () {
+                    print('button pressed!');
+                  },
+                  icon: const Icon(Icons.photo_camera),
+                  label: const Text('TAKE PHOTO'),
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color.fromARGB(255, 4, 103, 209),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(16.0)),
+                      minimumSize:
+                          Size(1, MediaQuery.of(context).size.height * 0.05),
+                      textStyle: const TextStyle(fontSize: 20) //////// HERE
+                      ),
+                ),
+              ])
+            ],
+          ),
+        ));
   }
 }
